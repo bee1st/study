fac0d291cf907ede8de93142859b2f0c35d4fa28
Junwoo Park <beebee0819@gmail.com>
2020-11-11 17:17:45 +0900

    make inside search file program to java

--- remind/src/Account/Cmd.java
+++ remind/src/Account/Cmd.java
@@ -1,23 +1,30 @@
 package Account;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
-//C:\Windows\System32
 public class Cmd {
 
-	public static void main(String[] args) {
-		Runtime rt = Runtime.getRuntime();
-		String exeFile = "C:\\Windows\\System32\\cmd.exe";
-		System.out.println("exeFile: " + exeFile);
-		Process p;
+	public static void main(String[] args) throws IOException {
+		
+		InputStream is = System.in; 
+		byte[] datas = new byte[100];
+		System.out.print("검색(ex c:\\, D:\\study, ..) : ");
+		
+		int nameBytes = is.read(datas);
+		String name = new String(datas, 0, nameBytes-2);
+		
+		File dir = new File(name);
 
-		try {
-			p = rt.exec(exeFile);
-			p.waitFor();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if(dir.exists()) {
+			String[] list = dir.list();
+			
+			for(String fileName : list) {
+				System.out.println(fileName);
+			} 
+		} else {
+			System.out.println("파일이 없습니다");
 		}
 	}
 }
